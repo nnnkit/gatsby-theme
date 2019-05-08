947bb2caa44e595b0bff9814f0b3316c899bc46d
nnnkit <iankit17@gmail.com>
2019-05-08 20:47:39 +0530

    add posts filesystem

--- packages/gatsby-theme-startgatsby/gatsby-config.js
+++ packages/gatsby-theme-startgatsby/gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
       }
     },
     {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts", // name of the folder
+        path: `${__dirname}/posts/` // path of url
+      }
+    },
+    {
       resolve: "gatsby-plugin-page-creator",
       options: {
         path: `${__dirname}/src/pages`
