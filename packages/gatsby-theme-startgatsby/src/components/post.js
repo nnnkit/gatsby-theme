0dc3a566869abe6997b02b9dcefbf1a776a8c440
nnnkit <iankit17@gmail.com>
2019-05-08 22:56:40 +0530

    add image on single post

--- packages/gatsby-theme-startgatsby/src/components/post.js
+++ packages/gatsby-theme-startgatsby/src/components/post.js
@@ -3,12 +3,22 @@ import { Link, graphql } from "gatsby";
 import { css } from "@emotion/core";
 import { MDXRenderer } from "gatsby-mdx";
 import Layout from "./layout";
+import Img from "gatsby-image";
 
 export const query = graphql`
   query($slug: String!) {
     mdx(frontmatter: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        keywords
+        banner {
+          sharp: childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+        categories
       }
       code {
         body
@@ -27,6 +37,10 @@ export default function post({ data: { mdx: post } }) {
       >
         <span role="img">👈</span>
       </Link>
+      <Img
+        fluid={post.frontmatter.banner.sharp.fluid}
+        alt={post.frontmatter.title}
+      />
       <MDXRenderer>{post.code.body}</MDXRenderer>
     </Layout>
   );
