bedd2572f9de50cac3ba1a0c579ec75a4d3bf487
nnnkit <iankit17@gmail.com>
2019-05-08 22:51:08 +0530

    add single blog post

--- packages/gatsby-theme-startgatsby/src/components/post.js
+++ packages/gatsby-theme-startgatsby/src/components/post.js
@@ -1,5 +1,33 @@
 import React from "react";
+import { Link, graphql } from "gatsby";
+import { css } from "@emotion/core";
+import { MDXRenderer } from "gatsby-mdx";
+import Layout from "./layout";
 
-export default function post() {
-  return <div>Post</div>;
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      code {
+        body
+      }
+    }
+  }
+`;
+export default function post({ data: { mdx: post } }) {
+  return (
+    <Layout>
+      <Link
+        to="/posts"
+        css={css`
+          font-size: 40px;
+        `}
+      >
+        <span role="img">👈</span>
+      </Link>
+      <MDXRenderer>{post.code.body}</MDXRenderer>
+    </Layout>
+  );
 }
