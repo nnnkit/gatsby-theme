b82041b12aa39986d4d8019b31c199ef75eb26b2
nnnkit <iankit17@gmail.com>
2019-06-04 11:54:18 +0530

    webpack loaders for npm packages

--- packages/gatsby-theme-startgatsby/gatsby-node.js
+++ packages/gatsby-theme-startgatsby/gatsby-node.js
@@ -1,3 +1,24 @@
+/**
+ * When shipping NPM modules, they typically need to be either
+ * pre-compiled or the user needs to add bundler config to process the
+ * files. Gatsby lets us ship the bundler config *with* the theme, so
+ * we never need a lib-side build step.  Since we dont pre-compile the
+ * theme, this is how we let webpack know how to process files.
+ */
+exports.onCreateWebpackConfig = ({ stage, loaders, plugins, actions }) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.dirname(require.resolve("gatsby-theme-startgatsby")),
+          use: [loaders.js()]
+        }
+      ]
+    }
+  });
+};
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     query {
